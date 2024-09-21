1d933a644b9ffd020e9d9e4dffe20ccb9e671eb2
Pinku2024 <pinku@multivariate.tech>
2024-09-21 19:19:10 +0530

    added: next tool page

---
+++ pages/next-tools.js
@@ -0,0 +1,163 @@
+import Link from "next/link";
+import dynamic from "next/dynamic";
+import Image from "next/image";
+const PageHead = dynamic(() => import("../components/elements/PageHead"));
+const Layout = dynamic(() => import("../components/layout/Layout"));
+
+const GscPagesInsights = () => {
+    return (
+        <>
+            <PageHead
+                title={"Next Tool"}
+                canonical={"https://nextgrowthlabs.com/gsc-page-insights"}
+            />
+
+            <Layout>
+                <div className="container mt-40">
+                    <Image src="/assets/imgs/page/next-tool/next-tool.webp" alt="Next Tool" width={1000} height={450} />
+                    <h1 className="font-4xl-bold color-brand-1 mb-20 mt-10">Next Tool</h1>
+                    <p className="font-md color-grey-400 mb-20">
+                        Welcome to <b>Next Tool!</b> Whether you’re a digital marketer, content creator, or website owner, this tool is designed to elevate your website analysis experience. With its user-friendly interface, you can easily merge data from Google Search Console and Google Analytics, gaining valuable insights into your site’s performance. Effortlessly track top-performing pages, analyze organic traffic, and enhance your content strategy with our intuitive data table. Dive into a more insightful and data-driven approach to optimizing your online presence today!
+                    </p>
+                    <h3 className="color-brand-1 mb-10">
+                        Overview
+                    </h3>
+                    <p className="font-md color-grey-400 mb-20">
+                        Next Tool is a powerful analytics tool that seamlessly combines data from Google Search Console and Google Analytics to provide comprehensive insights into your website’s performance. With an intuitive interface, users can easily explore a dynamic data table showcasing key metrics, such as top-performing pages, organic traffic, and engagement metrics. This tool empowers webmasters and marketers to make informed decisions, optimize their content strategy, and enhance overall website visibility. Uncover valuable insights and drive your online success with Next Tool.
+                    </p>
+                    <h3 className="color-brand-1 mb-20">Key Features:</h3>
+                    <ul style={{ listStyle: "disc" }} className="color-brand-1">
+                        <li className="font-md color-grey-400 mb-10 ml-20">
+                            <p className="font-md color-grey-400 mb-10">
+                                <span className="font-md-bold">Seamless Data Integration: </span>
+                                Effortlessly combine data from Google Search Console and Google Analytics to gain a holistic view of your website's performance.
+                            </p>
+                        </li>
+                        <li className="font-md color-grey-400 mb-10 ml-20">
+                            <p className="font-md color-grey-400 mb-10">
+                                <span className="font-md-bold mb-10">Interactive Data Table: </span>
+                                Explore an intuitive and customizable data table that displays key metrics, making it easy to identify trends and insights.
+                            </p>
+                        </li>
+                        <li className="font-md color-grey-400 mb-10 ml-20">
+                            <p className="font-md color-grey-400 mb-10">
+                                <span className="font-md-bold mb-10">Top-Performing Pages Analysis: </span>
+                                Quickly identify your best-performing pages based on organic traffic, engagement metrics, and more.
+                            </p>
+                        </li>
+                        <li className="font-md color-grey-400 mb-10 ml-20">
+                            <p className="font-md color-grey-400 mb-10">
+                                <span className="font-md-bold mb-10">Real-Time Insights: </span>
+                                Access up-to-date data for timely decision-making, allowing you to adapt your content strategy effectively.
+                            </p>
+                        </li>
+                        <li className="font-md color-grey-400 mb-10 ml-20">
+                            <p className="font-md color-grey-400 mb-10">
+                                <span className="font-md-bold mb-10">User-Friendly Interface: </span>
+                                Enjoy an easy-to-navigate platform designed for users of all skill levels, from beginners to seasoned professionals.
+                            </p>
+                        </li>
+                    </ul>
+                    {/* benefits */}
+                    <h3 className="color-brand-1 mb-20">Benefits:</h3>
+                    <ul style={{ listStyle: "disc" }} className="color-brand-1">
+                        <li className="font-md color-grey-400 mb-10 ml-20">
+                            <p className="font-md color-grey-400 mb-10">
+                                <span className="font-md-bold">Enhanced Website Performance Analysis: </span>
+                                Gain a deeper understanding of your site’s strengths and weaknesses through integrated data insights, helping you make informed optimizations.
+                            </p>
+                        </li>
+                        <li className="font-md color-grey-400 mb-10 ml-20">
+                            <p className="font-md color-grey-400 mb-10">
+                                <span className="font-md-bold mb-10">Informed Content Strategy: </span>
+                                Identify which pages drive the most traffic and engagement, enabling you to refine your content strategy and prioritize your efforts effectively.
+                            </p>
+                        </li>
+                        <li className="font-md color-grey-400 mb-10 ml-20">
+                            <p className="font-md color-grey-400 mb-10">
+                                <span className="font-md-bold mb-10">Optimized Marketing Campaigns: </span>
+                                Utilize insights from both Search Console and Google Analytics to improve your marketing initiatives, ensuring your efforts reach the right audience.
+                            </p>
+                        </li>
+                    </ul>
+                    {/* get started */}
+                    <h3 className="color-brand-1 mb-20">Get Started:</h3>
+                    <ul style={{ listStyle: "disc" }} className="color-brand-1">
+                        <li className="font-md color-grey-400 mb-10 ml-20">
+                            <p className="font-md color-grey-400 mb-10">
+                                <span className="font-md-bold">Authentication: </span>
+                                Begin by clicking the Google Auth button to grant read-only access to your Google Search Console and Google Analytics accounts.
+                            </p>
+                        </li>
+                        <li className="font-md color-grey-400 mb-10 ml-20">
+                            <p className="font-md color-grey-400 mb-10">
+                                <span className="font-md-bold">Select Your Sites: </span>
+                                In the second step, choose from a list of your verified Google Search Console sites using the provided checkboxes.
+                            </p>
+                        </li>
+                        <li className="font-md color-grey-400 mb-10 ml-20">
+                            <p className="font-md color-grey-400 mb-10">
+                                <span className="font-md-bold">Select GA Properties: </span>
+                                Proceed to the third step to view and select your Google Analytics properties, also using checkboxes.
+                            </p>
+                        </li>
+                        <li className="font-md color-grey-400 mb-10 ml-20">
+                            <p className="font-md color-grey-400 mb-10">
+                                <span className="font-md-bold">Sync Your Data: </span>
+                                After clicking the "Finish" button, your selected site and GA property will be synced with our database.
+                            </p>
+                        </li>
+                        <li className="font-md color-grey-400 mb-20 ml-20">
+                            <p className="font-md color-grey-400 mb-10">
+                                <span className="font-md-bold">Data Retrieval: </span>
+                                With the selected site and GA property now rendered in separate dropdowns, choose your options and click the "Get Site Data" button to generate a combined data table with insights from both Google Search Console and Google Analytics for easy review.
+                            </p>
+                        </li>
+                    </ul>
+                    <h3 className="color-brand-1 mb-10">Get Started Today!</h3>
+                    <p className="font-md color-grey-400 mb-20">
+                        Ready to elevate your website analysis with powerful insights? Access Next Tool now and transform the way you understand and optimize your site’s performance. Unlock the full potential of your data by seamlessly combining Google Search Console and Google Analytics metrics. Make informed decisions, enhance your content strategy, and drive your online success with ease!
+                    </p>
+                    <h3 className="color-brand-1 mb-10">Privacy:</h3>
+                    <p className="font-md color-grey-400 mb-20">
+                        ✅ We are committed to ensuring that Next Tool does not collect or misuse your personal data. To learn more, please see our comprehensive{" "}<Link className="utm-link font-md-bold" href="/privacy-policy">
+                            privacy policy.
+                        </Link>
+                    </p>
+                    <p className="font-md color-grey-400 mb-10">We declare that your data is:</p>
+                    <ul style={{ listStyle: "disc" }} className="color-brand-1">
+                        <li className="font-md color-grey-400 mb-10 ml-20">
+                            <p className="font-md color-grey-400 mb-10">
+                                Not being sold to third parties, outside of the approved use cases.
+                            </p>
+                        </li>
+                        <li className="font-md color-grey-400 mb-10 ml-20">
+                            <p className="font-md color-grey-400 mb-10">
+                                Not being used or transferred for purposes unrelated to the tool's core functionality.
+                            </p>
+                        </li>
+                        <li className="font-md color-grey-400 mb-10 ml-20">
+                            <p className="font-md color-grey-400 mb-10">
+                                Not being used or transferred to determine creditworthiness or for lending purposes.
+                            </p>
+                        </li>
+                        <li className="font-md color-grey-400 mb-10 ml-20">
+                            <p className="font-md color-grey-400 mb-10">
+                                Not being shared with any third-party <b>AI tools, AI models, or external parties.</b>
+                            </p>
+                        </li>
+                        <h3 className="color-brand-1 mb-10">Contact Us:</h3>
+                        <p className="font-md color-grey-400 mb-10">
+                            For any questions, feedback, or support related to the  <b>Next Tool</b>, please don't hesitate to contact us at <Link className="utm-link font-md-bold" href="/contact">
+                                https://nextgrowthlabs.com
+                            </Link>
+                        </p>
+                    </ul>
+
+                </div>
+            </Layout>
+        </>
+    );
+};
+
+export default GscPagesInsights;
