171e259b9766f336f0a1aacc2af40719ea1f9f7c
Nirmal-MultiVariate <nirmal@multivariate.tech>
2024-09-28 12:04:28 +0530

    added notice

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -4,7 +4,7 @@ import Header from "./Header";
 import Sidebar from "./Sidebar";
 import Preloader from "../elements/Preloader";
 import Footer from "./Footer";
-import { ToastContainer } from "react-toastify"
+import { ToastContainer } from "react-toastify";
 
 import { useRouter } from "next/router";
 const Layout = ({ children }) => {
@@ -41,6 +41,20 @@ const Layout = ({ children }) => {
   };
   return (
     <>
+      <div className="text-center bg-brand-1 pt-20 pb-20 color-white">
+        <h6 className="color-white">Important Notice</h6>
+        <p>
+          It has come to our attention that certain websites are falsely
+          claiming to have ties or affiliations with us, especially regarding
+          remote work opportunities.
+        </p>
+        <p>
+          We would like to clarify that **we have no association** with any such
+          sites or entities. Please verify any information directly with us
+          through our official website to avoid potential scams or
+          misinformation.
+        </p>
+      </div>
       <Header
         handleOpen={handleOpen}
         handleRemove={handleRemove}
@@ -51,7 +65,7 @@ const Layout = ({ children }) => {
       {loading ? <Preloader /> : <main className="main">{children}</main>}
       <Footer />
       <BackToTop />
-      <ToastContainer style={{ zIndex: "99999999999999999" }}/>
+      <ToastContainer style={{ zIndex: "99999999999999999" }} />
     </>
   );
 };
