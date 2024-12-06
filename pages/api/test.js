34fa3cf5efe70dcd49404ac69ec0f65d34e4b00f
Pinku2024 <pinku@multivariate.tech>
2024-12-06 16:44:29 +0530

    test env runtime edge

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,9 +1,11 @@
 const userName = process.env.JS_API_USER_NAME || "Not Set";
 const password = process.env.JS_API_PASSWORD || "Not Set";
-
+export const runtime = "edge"
 export default function handler(req, res) {
     console.log("Username:", userName);
     console.log("Password:", password);
-
-    res.status(200).json({ message: "Check logs", userName, password });
+    return new Response(JSON.stringify({ message: "Check logs", userName, password }), {
+        status: 200,
+        headers: { "Content-Type": "application/json" },
+    })
 }
