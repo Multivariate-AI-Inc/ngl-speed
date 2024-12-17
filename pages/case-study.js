3b392e184446ebd1905542feb964e97d50e8e9b1
Pinku <pinku@multivariate.tech>
2024-12-17 19:20:11 +0530

    added case study

--- pages/case-study.js
+++ pages/case-study.js
@@ -3,6 +3,7 @@ import dynamic from "next/dynamic";
 import Image from "next/image";
 const PageHead = dynamic(() => import("../components/elements/PageHead"));
 const Layout = dynamic(() => import("../components/layout/Layout"));
+const Contact = dynamic(()=> import("../components/home/Contact"))
 
 const CaseStudy = () => {
     return (
@@ -43,7 +44,7 @@ const CaseStudy = () => {
                     <div className="container">
                         <div className="row">
                             <div className="col-lg-12 text-center">
-                                <h2 className="color-brand-1 mb-20">Lastest Articless</h2>
+                                <h2 className="color-brand-1 mb-20">Lastest Articles</h2>
                             </div>
                         </div>
                         
@@ -54,11 +55,11 @@ const CaseStudy = () => {
                                 <div className="col-lg-6 col-md-6 mb-30 item-article featured">
                                     <div className="card-blog-grid card-blog-grid-3 hover-up">
                                         <div className="card-image"><Link href="/blog-detail"><img src="assets/imgs/page/blog/img1.png" alt="iori" /></Link>
-                                            <Link href="blog"><label className="lbl-border">Marketting</label></Link>
+                                            <Link href="#"><label className="lbl-border">Marketing</label></Link>
                                         </div>
                                         <div className="card-info"><Link href="/blog-detail">
                                             <h4 className="color-brand-1">How to save money - 8 simple ways to start saving money every month</h4></Link>
-                                            <div className="mb-25 mt-10"><span className="font-xs color-grey-500">November 17, 2022</span><span className="font-xs color-grey-500 icon-read">2 min read</span></div>
+                                            {/* <div className="mb-25 mt-10"><span className="font-xs color-grey-500">November 17, 2022</span><span className="font-xs color-grey-500 icon-read">2 min read</span></div> */}
                                             <p className="font-sm color-grey-500 mt-20">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit laborum — semper quis lectus nulla. Interactively transform magnetic growth strategies whereas prospective "outside the box" thinking.</p>
                                         </div>
                                     </div>
@@ -66,11 +67,11 @@ const CaseStudy = () => {
                                 <div className="col-lg-6 col-md-6 mb-30 item-article guides">
                                     <div className="card-blog-grid card-blog-grid-3 hover-up">
                                         <div className="card-image"><Link href="/blog-detail"><img src="assets/imgs/page/blog/img2.png" alt="iori" /></Link>
-                                        <Link href="blog"><label className="lbl-border">Technology</label></Link>
+                                        <Link href="#"><label className="lbl-border">Technology</label></Link>
                                         </div>
                                         <div className="card-info"><Link href="/blog-detail">
                                             <h4 className="color-brand-1">Essential Steps to Building And Refining an Effective UX Portfolio</h4></Link>
-                                            <div className="mb-25 mt-10"><span className="font-xs color-grey-500">November 17, 2022</span><span className="font-xs color-grey-500 icon-read">2 min read</span></div>
+                                            {/* <div className="mb-25 mt-10"><span className="font-xs color-grey-500">November 17, 2022</span><span className="font-xs color-grey-500 icon-read">2 min read</span></div> */}
                                             <p className="font-sm color-grey-500 mt-20">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit laborum — semper quis lectus nulla. Interactively transform magnetic growth strategies whereas prospective "outside the box" thinking.</p>
                                         </div>
                                     </div>
@@ -78,11 +79,11 @@ const CaseStudy = () => {
                                 <div className="col-lg-6 col-md-6 mb-30 item-article guides">
                                     <div className="card-blog-grid card-blog-grid-3 hover-up">
                                         <div className="card-image"><Link href="/blog-detail"><img src="assets/imgs/page/blog/img3.png" alt="iori" /></Link>
-                                        <Link href="blog"><label className="lbl-border">Marketing</label></Link>
+                                        <Link href="#"><label className="lbl-border">Marketing</label></Link>
                                         </div>
                                         <div className="card-info"><Link href="/blog-detail">
                                             <h4 className="color-brand-1">10 Content Proofreading Tips to Catch More Avoidable Goofs</h4></Link>
-                                            <div className="mb-25 mt-10"><span className="font-xs color-grey-500">November 17, 2022</span><span className="font-xs color-grey-500 icon-read">2 min read</span></div>
+                                            {/* <div className="mb-25 mt-10"><span className="font-xs color-grey-500">November 17, 2022</span><span className="font-xs color-grey-500 icon-read">2 min read</span></div> */}
                                             <p className="font-sm color-grey-500 mt-20">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit laborum — semper quis lectus nulla. Interactively transform magnetic growth strategies whereas prospective "outside the box" thinking.</p>
                                         </div>
                                     </div>
@@ -90,11 +91,11 @@ const CaseStudy = () => {
                                 <div className="col-lg-6 col-md-6 mb-30 item-article guides">
                                     <div className="card-blog-grid card-blog-grid-3 hover-up">
                                         <div className="card-image"><Link href="/blog-detail"><img src="assets/imgs/page/blog/img4.png" alt="iori" /></Link>
-                                        <Link href="blog"><label className="lbl-border">Tutorial</label></Link>
+                                        <Link href="#"><label className="lbl-border">Tutorial</label></Link>
                                         </div>
                                         <div className="card-info"><Link href="/blog-detail">
                                             <h4 className="color-brand-1">Everything a Beginner Blogger Needs to Know for 2023</h4></Link>
-                                            <div className="mb-25 mt-10"><span className="font-xs color-grey-500">November 17, 2022</span><span className="font-xs color-grey-500 icon-read">2 min read</span></div>
+                                            {/* <div className="mb-25 mt-10"><span className="font-xs color-grey-500">November 17, 2022</span><span className="font-xs color-grey-500 icon-read">2 min read</span></div> */}
                                             <p className="font-sm color-grey-500 mt-20">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit laborum — semper quis lectus nulla. Interactively transform magnetic growth strategies whereas prospective "outside the box" thinking.</p>
                                         </div>
                                     </div>
@@ -105,6 +106,7 @@ const CaseStudy = () => {
                         </div>
                     </div>
                 </section>
+                <Contact />
             </Layout>
         </>
     );
